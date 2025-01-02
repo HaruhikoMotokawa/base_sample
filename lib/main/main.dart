22cd2ad24b93e01828ddd81668340b2d408d9467
本川　晴彦 <harx6009@icloud.com>
2025-01-02 13:45:03 +0900

    feat: main.dartを削除し、HomeViewModelを新規作成してアプリの構造を整理

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,7 +1,7 @@
-import 'package:base_sample/main.dart';
 import 'package:base_sample/main/app_startup/consumer.dart';
 import 'package:base_sample/main/main_app/error.dart';
 import 'package:base_sample/main/main_app/loading.dart';
+import 'package:base_sample/main/main_app/main_app.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
