2d3fb2e20d6d56d33a88a89efefe1c4a558418af
本川　晴彦 <harx6009@icloud.com>
2025-01-02 17:37:50 +0900

    feat: アプリ名を'Base Sample'から'NewAppName'に変更

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 class Constants {
   // INFO: アプリ名はここで変更
-  static const appName = 'Base Sample';
+  static const appName = 'NewAppName';
   static const baseSeedColor = Color.fromARGB(255, 98, 0, 255);
 }
